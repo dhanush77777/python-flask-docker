08da7eeab3286f2a9140a9a627695ba151ce9df9
ig_Dhanush <66821432+dhanush77777@users.noreply.github.com>
2023-05-13 15:31:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Dockerizing the flask app"
+    return "Dockerizing the flask app v1"
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=int("3000"),debug=True)
